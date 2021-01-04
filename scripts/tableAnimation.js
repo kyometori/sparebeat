390528cb377706742859026cbedf5ca5580ce84d
kyometori <ac0xrpfs001@vivaldi.net>
2021-01-04 13:59:04 +0800

    more animation

---
+++ scripts/tableAnimation.js
@@ -0,0 +1,20 @@
+let i = 0;
+
+$(document).ready(function(){
+  $(".main h1 span").text("▼");
+
+  $("thead").click(function() {
+    $(this).parent().children("tbody").toggle(500);
+  });
+
+  $(".main h1").click(function() {
+    $(this).next("table").animate({height: 'toggle', opacity: 'toggle'});
+    $(this).children(".arrow").text() === "▼" ? $(this).children(".arrow").text("▲")
+                                              : $(this).children(".arrow").text("▼");
+  });
+
+  $(".title").dblclick(function() {
+    i >= 10 ? alert('太閒喔') : $(".main").animate({height: 'toggle', opacity: 'toggle'});
+    i++;
+  });
+});
