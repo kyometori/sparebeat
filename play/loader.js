e9df41471d4c04c3071ca46b00bd576704e0833b
kyometori <ac0xrpfs001@vivaldi.net>
2021-02-09 23:35:55 +0800

    if failed then remove timeline

--- play/loader.js
+++ play/loader.js
@@ -2,7 +2,7 @@ Sparebeat.autoload();
 const hash = md5(location.origin + "/");
 const timelineURL = "https://sparebeat.com/embed/timeline/" + hash;
 const timeline = document.getElementById('timeline');
-timeline.setAttribute('src', timelineURL);
+timeline.src = timelineURL;  
 window.addEventListener('load', function () {
   document.getElementById('sparebeat').height = window.outerWidth < 990 ? 1280 : 640;
 });
