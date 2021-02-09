930b8f3f6902c85bc46a1f09929e7205584693b4
kyometori <ac0xrpfs001@vivaldi.net>
2021-02-09 23:01:15 +0800

    timeline test

--- play/loader.js
+++ play/loader.js
@@ -1,4 +1,7 @@
 Sparebeat.autoload();
+var hash = md5(location.origin + "/");
+var timelineURL = "https://sparebeat.com/embed/timeline/" + hash;
+document.getElementById('timeline').src = timelineURL;
 window.addEventListener('load', function () {
   document.getElementById('sparebeat').height = window.outerWidth < 990 ? 1280 : 640;
 });
