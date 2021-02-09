e48c7f43dad9cf9bd544255ef416249aeacdac1b
kyometori <ac0xrpfs001@vivaldi.net>
2021-02-09 23:38:27 +0800

    timeline removing

--- play/loader.js
+++ play/loader.js
@@ -1,8 +1,4 @@
 Sparebeat.autoload();
-const hash = md5(location.origin + "/");
-const timelineURL = "https://sparebeat.com/embed/timeline/" + hash;
-const timeline = document.getElementById('timeline');
-timeline.src = timelineURL;  
 window.addEventListener('load', function () {
   document.getElementById('sparebeat').height = window.outerWidth < 990 ? 1280 : 640;
 });
